12c0fc930d554df795e5fc232d433ce3b690be02
fede <ffigueroa.dev@gmail.com>
2019-10-25 22:41:30 -0300

    Subiendo a ambos firebase

--- src/app/members/dashboard/dashboard.page.ts
+++ src/app/members/dashboard/dashboard.page.ts
@@ -105,13 +105,13 @@ export class DashboardPage implements OnInit {
     });
   }
 
-  takeUploadPicture(pt) {
+  takeUploadPicture(isBeauty) {
 
     this.camera.getPicture(this.getCameraOptions()).then(imagePath => {
       console.log("Ya capturo la foto: ", imagePath);
       var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
       var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
-      this.saveAndUploadNewPhoto(correctPath, currentName, this.createNewPhotoName(), pt);
+      this.saveAndUploadNewPhoto(correctPath, currentName, this.createNewPhotoName(), isBeauty);
     });
   }
 
@@ -152,11 +152,11 @@ export class DashboardPage implements OnInit {
     return newFileName;
   }
 
-  saveAndUploadNewPhoto(namePath, currentName, newFileName, pt) {
+  saveAndUploadNewPhoto(namePath, currentName, newFileName, isBeauty) {
     console.log("Subiendo foto 1");
     this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(success => {
       console.log("Aparentemente se guardo bien en el filesystem, vamos a subirla..");
-      this.sendPhotoToFirestore(this.getFilePath(newFileName), pt);
+      this.sendPhotoToFirestore(this.getFilePath(newFileName), isBeauty);
     }, error => {
       console.log("Error al guardar foto tomada");
       this.presentToast('Error guardando foto tomada.');
@@ -168,25 +168,24 @@ export class DashboardPage implements OnInit {
     return this.file.dataDirectory + name;
   }
 
-  sendPhotoToFirestore(filePath, pt) {
+  sendPhotoToFirestore(filePath, isBeauty) {
     console.log("Mandando foto...");
     this.file.resolveLocalFilesystemUrl(filePath)
       .then(entry => {
-        (<FileEntry>entry).file(file => this.uploadPhoto(file, pt));
+        (<FileEntry>entry).file(file => this.uploadPhoto(file, isBeauty));
       })
       .catch(err => {
         this.presentToast('Error leyendo foto guardada.');
       });
   }
 
-  uploadPhoto(file: any, pt) {
+  uploadPhoto(file: any, isBeauty) {
     console.log("Armando BLOB");
     const reader = new FileReader();
     reader.onloadend = () => {
       const imgBlob = new Blob([reader.result], {
         type: ".jpg"
       });
-      reader.readAsArrayBuffer(file);
       this.presentToast('Foto subida con exito.');
       const path = `buildingStorage/${new Date().getTime()}_${file.name}`;
 
@@ -197,37 +196,31 @@ export class DashboardPage implements OnInit {
       this.task = this.fireStorage.upload(path, imgBlob);
       console.log("Tarea obtenida")
 
-      this.snapshot = this.task.snapshotChanges().pipe(
-        finalize(() => {
-          console.log("Finalizo el upload")
-          console.log("Obteniendo path de descarga del archivo")
-          // Get uploaded file storage path
-          this.UploadedFileURL = fileRef.getDownloadURL();
-  
-          console.log("Iniciando upload a la DB")
-          this.UploadedFileURL.subscribe(resp => {
-            this.addImagetoDB({
-              name: file.name,
-              filepath: resp,
-              size: this.fileSize,
-              votes: 0,
-              uploadInstant: Date.now(),
-              userName: "el usuario"
-            }, pt == "POSITIVE" ? this.beautyPhotosCollection : this.uglyPhotosCollection);
-            this.isUploading = false;
-            this.isUploaded = true;
-          }, error => {
-            console.log("Erorrr")
-            console.error(error);
-          })
-        }),
-        tap(snap => {
-          console.log("En el tap");
-          this.fileSize = snap.totalBytes;
-        }),
-      )
-      // this.deleteImage(file.name)
+      this.task.then(as=>{
+        console.log("Finalizo el upload")
+        console.log("Obteniendo path de descarga del archivo")
+        // Get uploaded file storage path
+        this.UploadedFileURL = fileRef.getDownloadURL();
+
+        console.log("Iniciando upload a la DB")
+        this.UploadedFileURL.subscribe(resp => {
+          this.addImagetoDB({
+            name: file.name,
+            filepath: resp,
+            votes: 0,
+            uploadInstant: Date.now(),
+            userName: "el usuario"
+          }, isBeauty ? this.beautyPhotosCollection : this.uglyPhotosCollection);
+          this.isUploading = false;
+          this.isUploaded = true;
+          this.deleteImage(file.name)
+        }, error => {
+          console.log("Erorrr")
+          console.error(error);
+        })
+      });
     };
+    reader.readAsArrayBuffer(file);
   }
 
   pathForImage(img) {
@@ -273,14 +266,14 @@ export class DashboardPage implements OnInit {
     });
   }
 
-  async selectImage(pt) {
+  async selectImage(isBeauty) {
     const actionSheet = await this.actionSheetController.create({
       header: "Select Image source",
       buttons: [
         {
           text: 'Use Camera',
           handler: () => {
-            this.takeUploadPicture(pt);
+            this.takeUploadPicture(isBeauty);
           }
         },
         {
@@ -307,4 +300,123 @@ export class DashboardPage implements OnInit {
     this.verMisFotos = false;
     this.tomarFoto = true;
   }
+
+
+
+
+  // constructor(private storage: AngularFireStorage, private database: AngularFirestore, private platform: Platform) {
+  //   this.isUploading = false;
+  //   this.isUploaded = false;
+  //   //Set collection where our documents/ images info will save
+  //   // this.imageCollection = database.collection<FireDTO>('freakyImages');
+
+  //   this.beautyPhotosCollection = database.collection<FireDTO>('beautyPhotos', ref => ref.orderBy('uploadInstant', 'desc'));
+  //   this.uglyPhotosCollection = database.collection<FireDTO>('uglyPhotos', ref => ref.orderBy('uploadInstant', 'desc'));
+
+  //   this.beautyPhotos = this.beautyPhotosCollection.snapshotChanges().pipe(map(actions => actions.map(a => {
+  //     const data = a.payload.doc.data() as FireDTO;
+  //     const documentId = a.payload.doc.id;
+  //     return { documentId, ...data };
+  //   })));
+
+  //   this.uglyPhotos = this.uglyPhotosCollection.snapshotChanges().pipe(
+  //     map(actions =>
+  //       actions.map(a => {
+  //         const data = a.payload.doc.data() as FireDTO;
+  //         const documentId = a.payload.doc.id;
+  //         return { documentId, ...data };
+  //       })
+  //     ));
+
+  // }
+
+
+  // uploadFile(event: FileList, isBeauty: boolean) {
+
+  //   // The File object
+  //   const file = event.item(0)
+
+  //   // Validation for Images Only
+  //   if (file.type.split('/')[0] !== 'image') {
+  //     console.error('unsupported file type :( ')
+  //     return;
+  //   }
+
+  //   this.isUploading = true;
+  //   this.isUploaded = false;
+
+
+  //   this.fileName = file.name;
+
+  //   // The storage path
+  //   const path = `buildingStorage/${new Date().getTime()}_${file.name}`;
+
+  //   // Totally optional metadata
+  //   const customMetadata = { app: 'Relevamiento en edificios' };
+
+  //   //File reference
+  //   const fileRef = this.storage.ref(path);
+
+  //   // The main task
+  //   this.task = this.storage.upload(path, file, { customMetadata });
+
+  //   // Get file progress percentage
+  //   this.percentage = this.task.percentageChanges();
+
+  //   this.snapshot = this.task.snapshotChanges().pipe(
+
+  //     finalize(() => {
+  //       // Get uploaded file storage path
+  //       this.UploadedFileURL = fileRef.getDownloadURL();
+
+  //       this.UploadedFileURL.subscribe(resp => {
+  //         this.addImagetoDB({
+  //           name: file.name,
+  //           filepath: resp,
+  //           size: this.fileSize,
+  //           votes: 0,
+  //           uploadInstant: Date.now(),
+  //           userName: "el usuario"
+  //         }, isBeauty ? this.beautyPhotosCollection : this.uglyPhotosCollection);
+  //         this.isUploading = false;
+  //         this.isUploaded = true;
+  //       }, error => {
+  //         console.error(error);
+  //       })
+  //     }),
+  //     tap(snap => {
+  //       this.fileSize = snap.totalBytes;
+  //     })
+  //   )
+  // }
+
+  // addImagetoDB(image: FireDTO, imagesCollection: AngularFirestoreCollection<FireDTO>) {
+  //   //Create an ID for document
+  //   const id = this.database.createId();
+
+  //   //Set document id with value in database
+  //   imagesCollection.doc(id).set(image).then(resp => {
+  //     console.log(resp);
+  //   }).catch(error => {
+  //     console.log("error " + error);
+  //   });
+  // }
+
+  // vote(docId, incrementVotes: boolean, photoType: PhotoType) {
+
+  //   const imageRef = photoType == PhotoType.POSITIVE ? this.beautyPhotosCollection.doc(docId).ref : this.uglyPhotosCollection.doc(docId).ref;
+
+  //   this.database.firestore.runTransaction(transaction => {
+  //     return transaction.get(imageRef).then(res => {
+  //       if (!res.exists) {
+  //         throw "Document does not exist!";
+  //       }
+
+  //       transaction.update(imageRef, {
+  //         votes: incrementVotes ? res.data().votes + 1 : res.data().votes - 1
+  //       });
+
+  //     })
+  //   });
+  // }
 }
